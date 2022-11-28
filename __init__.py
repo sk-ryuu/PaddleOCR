57f4154608dc60cc7ace4512ba929e95da308465
Sk-Ryuu <114046540+sk-ryuu@users.noreply.github.com>
2022-11-28 19:41:49 +0700

    feat: remove wrong directory __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .paddleocr import *
-from .textRecoginition import TextRecoginition
 __version__ = paddleocr.VERSION
 # __all__ = [
 #     'PaddleOCR', 'PPStructure', 'draw_ocr', 'draw_structure_result',
